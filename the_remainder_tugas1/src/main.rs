f7c75c7f2b66a2d27e5c9a570e81b90b09f14a14
Hekkura <54793875+Hekkura@users.noreply.github.com>
2022-10-19 17:02:23 +0700

    second commit
    scan support

--- the_remainder_tugas1/src/main.rs
+++ the_remainder_tugas1/src/main.rs
@@ -1,5 +1,7 @@
-fn modulo(a:i32,b:i32) -> i32 {
-    a%b
+use std::io;
+
+fn modulo(param1:i32,param2:i32) -> i32 {
+    param1 % param2
 }
 
 fn display_result(result: i32){
@@ -8,6 +10,32 @@ fn display_result(result: i32){
 }
 
 fn main(){
-    let result = modulo(10,3);
+    let mut input1 = String::new();
+
+    let mut input2 = String::new();
+
+    println!("=====Input Param1=====");
+
+    io::stdin()
+            .read_line(&mut input1)
+            .expect("Failed to read line");
+
+
+    println!("=====Input Param2=====");      
+    io::stdin()
+            .read_line(&mut input2)
+            .expect("Failed to read line");
+
+    let input_parsed1 = input1
+        .trim()
+        .parse()
+        .expect("Input not an integer");
+
+    let input_parsed2 = input2
+        .trim()
+        .parse()
+        .expect("Input not an integer");
+
+    let result = modulo(input_parsed1,input_parsed2);
     display_result(result);
 }
