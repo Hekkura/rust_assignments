46298fc741bb713018af52ce1338d6b3418a574c
Hekkura <54793875+Hekkura@users.noreply.github.com>
2022-10-19 18:06:43 +0700

    second commit
    scan support

--- concat_string_tugas1/src/main.rs
+++ concat_string_tugas1/src/main.rs
@@ -1,10 +1,29 @@
-// use std::io;
+use std::io;
 
-fn concat_string(a: &str, b: &str) {
-    let result =format!("{} {}", a, b);
+fn concat_string(param1: String, param2: String) {
+    let result =format!("{} {}", param1, param2);
     println!("{result}");
 }
 
 fn main() {
-    concat_string("I Love", "Rust");
+    let mut input1 = String::new();
+
+    let mut input2 = String::new();
+
+    println!("=====Input Param1=====");
+
+    io::stdin()
+            .read_line(&mut input1)
+            .expect("Failed to read line");
+
+    println!("=====Input Param2=====");        
+
+    io::stdin()
+            .read_line(&mut input2)
+            .expect("Failed to read line");
+
+    let input1_slice = String::from(input1.strip_suffix("\r\n").unwrap());
+    let input2_slice = String::from(input2.strip_suffix("\r\n").unwrap());
+
+    concat_string(input1_slice, input2_slice);
 }
